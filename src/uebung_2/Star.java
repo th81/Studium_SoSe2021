9a3f2296c2ca306735ab40a492c4aa36b0f287ea
HandoSan <60817123+HandoSan@users.noreply.github.com>
2021-05-21 10:54:06 +0200

    Update Star.java

--- src/uebung_2/Star.java
+++ src/uebung_2/Star.java
@@ -1,5 +1,10 @@
 package uebung_2;
 
 public class Star {
-	private StarDatabase database;
+	private StarsDatabase database;
+	private String name;
+	private String id;
+	private double distance;
+	private double apparentMagnitude;
+	private String type;
 }
